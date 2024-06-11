359f7eb5d934a238631d0b4771870bb1034ab684
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2024-06-11 14:24:52 +0700

    update scripts

--- load_data.py
+++ load_data.py
@@ -3,15 +3,12 @@ import requests
 # import config
 import pandas as pd
 
-
-
-
 import psycopg2
 
 with open('load_data.sql', 'r') as file:
-    sql_query = file.read()
+    sql_load= file.read()
 
-conn = psycopg2.connect(database = "test", 
+conn = psycopg2.connect(database = "test2", 
                         user = "postgres", 
                         host= 'localhost',
                         password = "170545Kean",
@@ -20,7 +17,7 @@ conn = psycopg2.connect(database = "test",
 # Open a cursor to perform database operations
 cur = conn.cursor()
 # Execute a command: create datacamp_courses table
-cur.execute(sql_query)
+cur.execute(sql_load)
 # Make the changes to the database persistent
 conn.commit()
 # Close cursor and communication with the database
--- load_data.sql
+++ load_data.sql
@@ -1,69 +1,81 @@
 BEGIN;
 
+DROP TABLE IF EXISTS public."CompanyData";
+DROP TABLE IF EXISTS public."Employee";
+DROP TABLE IF EXISTS public."JobProfile";
+DROP TABLE IF EXISTS public."State";
+DROP TABLE IF EXISTS public."Country";
+
+
 CREATE TABLE public."State"(
-    state "char" NOT NULL,
-    state_full "char" NOT NULL,
+    state char(2) NOT NULL,
+    state_full varchar(50) NOT NULL,
     PRIMARY KEY (state)
 );
-COPY public."State" FROM '' WITH (FORMAT csv, HEADER);
+COPY public."State" FROM '/Users/nipphittt/State.csv' WITH (FORMAT csv, HEADER);
+
 
 CREATE TABLE public."Country"(
-    country "char" NOT NULL,
-    country_full "char" NOT NULL,
+    country char(2) NOT NULL,
+    country_full varchar(20) NOT NULL,
     PRIMARY KEY (country)
 );
-COPY public."Country" FROM '' WITH (FORMAT csv, HEADER);
+COPY public."Country" FROM '/Users/nipphittt/Country.csv' WITH (FORMAT csv, HEADER);
+
 
 CREATE TABLE public."Employee"
 (
     employee_id INTEGER NOT NULL,
-    firstname "char" NOT NULL,
-    lastname "char" NOT NULL,
+    firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
     age INTEGER NOT NULL,
     dob date NOT NULL,
-    street_address "char" NOT NULL,
-    city "char" NOT NULL,
-    state "char" NOT NULL,
-    zipcode INTEGER NOT NULL,
-    country "char" NOT NULL,
+    street_address varchar(50),
+    city varchar(50),
+    state char(2),
+    zipcode varchar(8),
+    country char(2) NOT NULL,
 
     PRIMARY KEY(employee_id),
     FOREIGN KEY(state) REFERENCES public."State" (state),
     FOREIGN KEY(country) REFERENCES public."Country" (country)
 );
-COPY public."Employee" FROM '' WITH (FORMAT csv, HEADER);
+COPY public."Employee" FROM '/Users/nipphittt/Employee.csv' WITH (FORMAT csv, HEADER);
+
+
 
 CREATE TABLE public."JobProfile"
 (
-    jobprofile "char" NOT NULL,
-    jobtitle "char" NOT NULL,
-    department "char" NOT NULL,
+    job_profile char(7) NOT NULL,
+    job_title varchar(50) NOT NULL,
+    department varchar(20) NOT NULL,
     compensation FLOAT NOT NULL,
-    level "char" NOT NULL,
+    level varchar(50) NOT NULL,
     bonus_pct FLOAT NOT NULL,
-
-    PRIMARY KEY(jobprofile),
+    PRIMARY KEY (job_profile)
 );
-COPY public."JobProfile" FROM '' WITH (FORMAT csv, HEADER);
+
+COPY public."JobProfile" FROM '/Users/nipphittt/JobProfile.csv' WITH (FORMAT csv, HEADER);
 
 -- DROP TABLE IF EXISTS public."Bill";
 CREATE TABLE public."CompanyData"
 (
-    id??
+    company_id INTEGER NOT NULL,
     employee_id INTEGER NOT NULL,
-    office "char" NOT NULL,
+    office varchar(10) NOT NULL,
     start_date date NOT NULL,
     termination_date date NOT NULL,
-    office_type "char" NOT NULL,
-    currency "char" NOT NULL,
-    salary INTEGER NOT NULL,
+    office_type varchar(10) NOT NULL,
+    currency char(3) NOT NULL,
+    salary FLOAT NOT NULL,
     active_status INTEGER NOT NULL,
-    job_profile "char" NOT NULL,
+    job_profile char(7) NOT NULL,
 
+    PRIMARY KEY(company_id),
     FOREIGN KEY(employee_id) REFERENCES public."Employee" (employee_id),
     FOREIGN KEY(job_profile) REFERENCES public."JobProfile" (job_profile)
 );
 
-COPY public."CompanyData" FROM '' WITH (FORMAT csv, HEADER);
+COPY public."CompanyData" FROM '/Users/nipphittt/CompanyData.csv' WITH (FORMAT csv, HEADER);
 
 END;
